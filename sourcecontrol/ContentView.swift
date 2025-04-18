3ddded37e2fe245eb0ae502f1e7d29c07503372a
rizankhanal <rizankhanal@gmail.com>
2025-04-18 15:41:35 +0545

    firstCommit

--- sourcecontrol/ContentView.swift
+++ sourcecontrol/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
+                .font(.largeTitle)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("This is source control")
         }
         .padding()
+        
     }
 }
 
