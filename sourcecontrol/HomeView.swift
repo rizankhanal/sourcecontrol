c8081bba5dc2408eb31defde30b54ad18a68ab61
rizankhanal <rizankhanal@gmail.com>
2025-04-18 15:50:16 +0545

    Add Home View

---
+++ sourcecontrol/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  sourcecontrol
+//
+//  Created by Rizan Khanal on 18/04/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Image(systemName: "sun.max.fill")
+        Text("Screeen View")
+    }
+}
+#Preview {
+    HomeView()
+}
