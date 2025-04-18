0eab3bdfe92d77eeeb7506f813fd8fda8ed1b63d
rizankhanal <rizankhanal@gmail.com>
2025-04-18 10:18:11 +0545

    Initial Commit

---
+++ sourcecontrol/sourcecontrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  sourcecontrolApp.swift
+//  sourcecontrol
+//
+//  Created by Rizan Khanal on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct sourcecontrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sourcecontrolTests/sourcecontrolTests.swift
@@ -0,0 +1,17 @@
+//
+//  sourcecontrolTests.swift
+//  sourcecontrolTests
+//
+//  Created by Rizan Khanal on 18/04/2025.
+//
+
+import Testing
+@testable import sourcecontrol
+
+struct sourcecontrolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
